dd2ab4d72a1108f0bce94961ffb05efdd7a93de3
Yigit Sever <yigit@ceng.metu.edu.tr>
2021-04-09 20:21:32 +0300

    Remove unused code from filters

--- src/custom_filters.rs
+++ src/custom_filters.rs
@@ -10,11 +10,6 @@ pub fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = Infallible> + Clo
     warp::any().map(move || db.clone())
 }
 
-// Optional query params to allow pagination
-// pub fn list_options() -> impl Filter<Extract = (ListOptions,), Error = Rejection> + Clone {
-//     warp::query::<ListOptions>()
-// }
-
 // Accept only json encoded Transaction body and reject big payloads
 pub fn transaction_json_body() -> impl Filter<Extract = (Transaction,), Error = Rejection> + Clone {
     warp::body::content_length_limit(1024 * 32).and(warp::body::json())
