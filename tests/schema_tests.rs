fe2cd485ffed31e25d30a378dd20cba11b13dfbf
alpaylan <alpkeles99@gmail.com>
2021-04-14 01:39:29 +0300

    test functions are generated.

---
+++ tests/schema_tests.rs
@@ -0,0 +1,97 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn claims_serialize_correctly() {
+
+    }
+
+    #[test]
+    fn claims_deserialize_correctly() {
+
+    }
+
+    #[test]
+    fn transaction_serialize_correctly() {
+
+    }
+
+    #[test]
+    fn transaction_deserialize_correctly() {
+
+    }
+
+    #[test]
+    fn block_serialize_correctly() {
+
+    }
+
+    #[test]
+    fn block_deserialize_correctly() {
+
+    }
+
+    #[test]
+    fn block_deserialize_when_vec_emptpy() {
+
+    }
+
+    #[test]
+    fn naked_block_serialize_correctly() {
+
+    }
+
+    #[test]
+    fn naked_block_deserialize_correctly() {
+
+    }
+
+    #[test]
+    fn naked_block_deserialize_when_vec_emptpy() {
+
+    }
+
+    #[test]
+    fn user_serialize_correctly() {
+
+    }
+
+    #[test]
+    fn user_deserialize_correctly() {
+
+    }
+
+    #[test]
+    fn metu_id_serialize_correctly() {
+
+    }
+
+    #[test]
+    fn metu_id_deserialize_correctly() {
+
+    }
+
+    #[test]
+    fn auth_request_serialize_correctly() {
+
+    }
+
+    #[test]
+    fn auth_request_deserialize_correctly() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
